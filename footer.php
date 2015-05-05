aa84d8d9ee340f63df10db189f912ca1992a867c
Chance Corbeil <chance@Chances-MacBook-Pro-2.local>
2015-05-05 00:15:58 -0700

    Remove space and unwanted code.

--- footer.php
+++ footer.php
@@ -26,17 +26,6 @@
 			</a>
 		<?php } // end if ?>
 
-
-
-
-
-
-
-
-
-
-
-
 		<div class="wrap">
 
 			<div class="footerwidget col-md-4">
@@ -70,17 +59,6 @@
 		</footer><!-- #colophon -->
 	</div>
 </div><!-- #page -->
-
-
-<script>
-	(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-	(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-	m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-	})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-	ga('create', 'UA-60473271-1', 'auto');
-	ga('send', 'pageview');
-</script>
 
 <?php wp_head(); ?>
 
